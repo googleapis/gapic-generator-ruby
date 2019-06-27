0582b1a8da68d26c55347e59fc6cece181d858ae
Mike Moore <mike@blowmage.com>
2019-06-27 14:04:41 -0600

    Add English to errors

--- gapic-common/lib/google/gapic/errors.rb
+++ gapic-common/lib/google/gapic/errors.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "English"
+
 module Google
   module Gapic
     # Common base class for exceptions raised by GAX.
