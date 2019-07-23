3f370db05dcc3d859e6c44b2e5443c094e5921cd
Daniel Azuma <dazuma@google.com>
2019-07-23 14:25:02 -0700

    Update libraries to prepare for initial prerelease (#186)

--- gapic-common/lib/gapic/common/version.rb
+++ gapic-common/lib/gapic/common/version.rb
@@ -14,6 +14,6 @@
 
 module Gapic
   module Common
-    VERSION = "2.0.alpha.0.development".freeze
+    VERSION = "0.0.1.dev.1".freeze
   end
 end
--- shared/output/cloud/showcase/lib/google/showcase/v1alpha3/messaging/client.rb
+++ shared/output/cloud/showcase/lib/google/showcase/v1alpha3/messaging/client.rb
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "gapic"
+require "gapic/common"
 require "gapic/config"
 require "gapic/config/method"
 
