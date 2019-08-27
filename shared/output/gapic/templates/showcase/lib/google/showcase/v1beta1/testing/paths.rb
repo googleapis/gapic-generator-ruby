552548f9db04b290c5c33d2c1d7f44b5b28d4329
Joe Bolinger <jbolinger@google.com>
2019-08-26 17:23:37 -0700

    reverse conditional

--- shared/output/gapic/templates/showcase/lib/google/showcase/v1beta1/testing/paths.rb
+++ shared/output/gapic/templates/showcase/lib/google/showcase/v1beta1/testing/paths.rb
@@ -56,7 +56,7 @@ module Google
           # @return [String]
           def test_path session:, test:
             raise ArgumentError, "session is required" if session.nil?
-            raise ArgumentError, "session cannot contain /" unless %r{/}.match? session
+            raise ArgumentError, "session cannot contain /" if %r{/}.match? session
             raise ArgumentError, "test is required" if test.nil?
 
             "sessions/#{session}/tests/#{test}"
