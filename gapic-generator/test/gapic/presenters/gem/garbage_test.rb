e4370a91166d5a94ce0adff6d48417973cf3e3fd
Mike Moore <mike@blowmage.com>
2020-01-06 11:58:47 -0700

    Add iam dependency to gem output (#301)

--- gapic-generator/test/gapic/presenters/gem/garbage_test.rb
+++ gapic-generator/test/gapic/presenters/gem/garbage_test.rb
@@ -43,7 +43,9 @@ class GarbageGemPresenterTest < PresenterTest
     assert_equal ["GarbageService"], presenter.services.map(&:name)
     presenter.services.each { |sp| assert_kind_of ServicePresenter, sp }
 
-    assert_equal ["google/api/field_behavior.proto", "google/api/resource.proto", "google/protobuf/any.proto", "google/protobuf/empty.proto", "google/rpc/status.proto", "google/longrunning/operations.proto", "google/protobuf/timestamp.proto", "google/protobuf/duration.proto", "garbage/garbage.proto"], presenter.proto_files.map(&:name)
+    assert_equal ["google/api/field_behavior.proto", "google/api/resource.proto", "google/protobuf/any.proto", "google/protobuf/empty.proto", "google/rpc/status.proto", "google/longrunning/operations.proto", "google/protobuf/timestamp.proto", "google/protobuf/duration.proto", "google/iam/v1/policy.proto", "google/iam/v1/iam_policy.proto", "garbage/garbage.proto"], presenter.proto_files.map(&:name)
     presenter.proto_files.each { |fp| assert_kind_of FilePresenter, fp }
+
+    assert presenter.iam_dependency?
   end
 end
