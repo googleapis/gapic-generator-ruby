3c49cf5a4e80f1a1bf28f41ce9fd7d4a5f578fef
Daniel Azuma <dazuma@google.com>
2020-02-25 12:21:11 -0800

    fix: require the simplecov library explicitly in generated tests

--- shared/output/gapic/templates/garbage/test/so/much/trash/garbage_service_operations_test.rb
+++ shared/output/gapic/templates/garbage/test/so/much/trash/garbage_service_operations_test.rb
@@ -22,6 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+require "simplecov"
 require "minitest/autorun"
 
 require "gapic/grpc/service_stub"
