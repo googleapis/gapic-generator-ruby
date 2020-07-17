2fab2ba7e5cd0c42a0b901875f7b72ce9cb72fc3
Viacheslav Rostovtsev <virost@google.com>
2020-07-17 14:44:14 -0700

    fixes for the cr 3

--- rules_ruby_gapic/docker_testing/rundocker.sh
+++ rules_ruby_gapic/docker_testing/rundocker.sh
@@ -21,3 +21,4 @@ docker build -t gapic-generator-ruby-bazel -f rules_ruby_gapic/docker_testing/Do
 docker run -it --rm \
 --mount type=bind,source="$(pwd)",target='/src/gapic-generator-ruby' \
 gapic-generator-ruby-bazel
+/bin/bash
