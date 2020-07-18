cd2633931e93c80b5e26a8011194b707b4fe8ede
Viacheslav Rostovtsev <virost@google.com>
2020-07-17 17:06:10 -0700

    emptyline

--- rules_ruby_gapic/docker_testing/rundocker.sh
+++ rules_ruby_gapic/docker_testing/rundocker.sh
@@ -21,4 +21,5 @@ docker build -t gapic-generator-ruby-bazel -f rules_ruby_gapic/docker_testing/Do
 docker run -it --rm \
 --mount type=bind,source="$(pwd)",target='/src/gapic-generator-ruby' \
 gapic-generator-ruby-bazel
+
 /bin/bash
