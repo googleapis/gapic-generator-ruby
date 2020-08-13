c3d41602e243aba26f8df3a68f618b4d5db2029d
Viacheslav Rostovtsev <virost@google.com>
2020-08-12 18:20:17 -0700

    cleanup

--- rules_ruby_gapic/ruby_binary/check/rainbow_bundler_test/lib/version.rb
+++ rules_ruby_gapic/ruby_binary/check/rainbow_bundler_test/lib/version.rb
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# this file was intentionally left blank
+# This file intentionally left blank
