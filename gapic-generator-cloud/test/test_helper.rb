32e98def1421898c0eb3f520dd7ba59a2cf6ceef
Viacheslav Rostovtsev <virost@google.com>
2021-06-03 18:19:56 -0700

    beter tests

--- gapic-generator-cloud/test/test_helper.rb
+++ gapic-generator-cloud/test/test_helper.rb
@@ -18,6 +18,8 @@ $LOAD_PATH.unshift ::File.expand_path("../lib", __dir__)
 $LOAD_PATH.unshift ::File.expand_path("../../shared/test_resources", __dir__)
 require "gapic/schema/api"
 require "gapic/generator"
+require "gapic/generators/cloud_generator"
+
 require "action_controller"
 require "action_view"
 
@@ -25,34 +27,37 @@ require "minitest/autorun"
 require "minitest/focus"
 
 class GeneratorTest < Minitest::Test
+  ##
+  # @param service [Symbol]
+  # @return [String]
   def proto_input service
     File.binread "proto_input/#{service}_desc.bin"
   end
 
+  ##
+  ##
+  # @param service [Symbol]
+  # @return [Google::Protobuf::Compiler::CodeGeneratorRequest]
   def request service
     Google::Protobuf::Compiler::CodeGeneratorRequest.decode proto_input(service)
   end
 
+  ##
+  # @param service [Symbol]
+  # @return [Gapic::Schema::Api]
   def api service
     Gapic::Schema::Api.new request(service),
                            parameter_schema: Gapic::Generators::CloudGeneratorParameters.default_schema
   end
 
+  ##
+  # @param service [Symbol]
+  # @param filename [String]
+  # @return [String]
   def expected_content service, filename
     File.read "expected_output/#{service}/#{filename}"
   end
 end
 
-class PresenterTest < Minitest::Test
-  def proto_input service
-    File.binread "proto_input/#{service}_desc.bin"
-  end
-
-  def request service
-    Google::Protobuf::Compiler::CodeGeneratorRequest.decode proto_input(service)
-  end
-
-  def api service
-    Gapic::Schema::Api.new request(service)
-  end
+class PresenterTest < GeneratorTest
 end
