2f4527773c1c8a3719a069d644574f98bbf5ccd2
Viacheslav Rostovtsev <virost@google.com>
2021-10-12 12:05:57 -0700

    add locations placeholder

---
+++ shared/test_resources/google-cloud-location/lib/google/cloud/location.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+# This is a placeholder that will be removed
+# when google-cloud-locations gem is released
+
+module Google
+  module Cloud
+    module Location
+    end
+    module Locations
+      class Client
+      end
+    end
+  end
+end
