43658e9ceeb633076a248b17f02981a4f6a3a7b0
Viacheslav Rostovtsev <virost@google.com>
2021-11-16 13:44:48 -0800

    fixup

--- shared/output/gapic/templates/testing/lib/testing/grpc_service_config/service_with_retries/client.rb
+++ shared/output/gapic/templates/testing/lib/testing/grpc_service_config/service_with_retries/client.rb
@@ -140,7 +140,7 @@ module Testing
 
           @location_client = Google::Cloud::Location::Locations::Client.new do |config|
             config.credentials = credentials
-            config.quota_project_id = @quota_project_id
+            config.quota_project = @quota_project_id
             config.endpoint = @config.endpoint
           end
 
