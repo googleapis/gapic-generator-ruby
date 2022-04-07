2f823dddda1cb9610996e0ec9cb33df8111a1cf7
Daniel Azuma <dazuma@google.com>
2022-04-07 11:14:56 -0700

    chore: Release 0.11.1 (#737)

--- gapic-generator-cloud/lib/gapic/generator/cloud/version.rb
+++ gapic-generator-cloud/lib/gapic/generator/cloud/version.rb
@@ -18,7 +18,7 @@
 module Gapic
   module Generator
     module Cloud
-      VERSION = "0.11.0"
+      VERSION = "0.11.1"
     end
   end
 end
--- gapic-generator/lib/gapic/generator/version.rb
+++ gapic-generator/lib/gapic/generator/version.rb
@@ -16,6 +16,6 @@
 
 module Gapic
   module Generator
-    VERSION = "0.11.0"
+    VERSION = "0.11.1"
   end
 end
