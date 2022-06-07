9c262235db29f7f001e3d74d2853aa8408a55e04
Viacheslav Rostovtsev <58152857+viacheslav-rostovtsev@users.noreply.github.com>
2022-06-07 13:42:40 -0700

    fix: LRO-wrapped methods of the internal Operations client not working (#768)

--- shared/output/gapic/templates/showcase/lib/google/showcase/v1beta1/echo/operations.rb
+++ shared/output/gapic/templates/showcase/lib/google/showcase/v1beta1/echo/operations.rb
@@ -102,6 +102,9 @@ module Google
               channel_args: @config.channel_args,
               interceptors: @config.interceptors
             )
+
+            # Used by an LRO wrapper for some methods of this service
+            @operations_client = self
           end
 
           # Service calls
