764053ec2462192e2ee230690d7305db5de5b76c
Viacheslav Rostovtsev <virost@google.com>
2022-08-02 15:55:27 -0700

    style

--- gapic-common/lib/gapic/rest/grpc_transcoder.rb
+++ gapic-common/lib/gapic/rest/grpc_transcoder.rb
@@ -108,11 +108,9 @@ module Gapic
           uri_values = bind_uri_values! http_binding, request_hash
           next if uri_values.any? { |_, value| value.nil? }
 
-          if http_binding.body && http_binding.body != "*" && !(request.respond_to? http_binding.body.to_sym)
-            # Note that the body template can only point to a top-level field,
-            # so there is no need to split the path.
-            next
-          end
+          # Note that the body template can only point to a top-level field,
+          # so there is no need to split the path.
+          next if http_binding.body && http_binding.body != "*" && !(request.respond_to? http_binding.body.to_sym)
 
           method = http_binding.method
           uri = expand_template http_binding.template, uri_values
@@ -184,9 +182,10 @@ module Gapic
           # Note 1: body template can only point to a top-level field.
           # Note 2: The field that body template points to can be null, in which case
           # an empty string should be sent. E.g. `Compute.Projects.SetUsageExportBucket`.
-          if request.respond_to?(body_template.to_sym) && request.send(body_template.to_sym)
+          request_body_field = request.send body_template.to_sym if request.respond_to? body_template.to_sym
+          if request_body_field
             request_hash_without_uri.delete camel_name_for body_template
-            body = request.send(body_template.to_sym).to_json(emit_defaults: true)
+            body = request_body_field.to_json emit_defaults: true
           end
 
           query_params = build_query_params request_hash_without_uri
