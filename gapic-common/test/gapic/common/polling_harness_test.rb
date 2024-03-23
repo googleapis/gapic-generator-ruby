ab832c199f1364bd5bbff09396bbb6d77b663e10
André Andreassa <aandreassa@users.noreply.github.com>
2024-03-22 17:03:55 -0700

    feat(gapic-common): introduce gapic polling harness (#1057)

---
+++ gapic-common/test/gapic/common/polling_harness_test.rb
@@ -0,0 +1,104 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "gapic/common/polling_harness"
+require "gapic/common/retry_policy"
+
+# Test class for Gapic::Common::PollingHarness
+class PollingHarnessTest < Minitest::Test
+  def test_init
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 20
+    polling_harness = Gapic::Common::PollingHarness.new retry_policy: retry_policy
+    assert_equal retry_policy, polling_harness.retry_policy
+  end
+
+  def test_wait_perform_delay_once
+    should_wait = true
+    retry_policy_mock = Minitest::Mock.new
+    retry_policy_mock.expect :perform_delay!, nil, []
+    polling_harness = Gapic::Common::PollingHarness.new retry_policy: retry_policy_mock
+
+    polling_harness.wait do
+      if should_wait
+        should_wait = false
+        nil
+      else
+        should_wait
+      end
+    end
+    retry_policy_mock.verify
+    assert_equal false, should_wait
+  end
+
+  def test_wait_perform_delay_many
+    perform_delay_total = 5
+    retry_policy_mock = Minitest::Mock.new
+    polling_harness = Gapic::Common::PollingHarness.new retry_policy: retry_policy_mock
+
+    polling_harness.wait do
+      if perform_delay_total.positive?
+        retry_policy_mock.expect :perform_delay!, nil, []
+        perform_delay_total -= 1
+        nil
+      else
+        perform_delay_total
+      end
+    end
+    retry_policy_mock.verify
+    assert_equal 0, perform_delay_total
+  end
+
+  def test_wait_with_retriable_code
+    perform_delay_count = 0
+    retry_policy = Gapic::Common::RetryPolicy.new retry_codes: [GRPC::Core::StatusCodes::UNAVAILABLE]
+    retry_policy.define_singleton_method :perform_delay! do
+      perform_delay_count += 1
+    end
+    polling_harness = Gapic::Common::PollingHarness.new retry_policy: retry_policy
+
+    should_raise_error = true
+    polling_harness.wait do
+      if should_raise_error
+        should_raise_error = false
+        raise ::GRPC::Unavailable
+      else
+        should_raise_error
+      end
+    end
+    assert_equal 1, perform_delay_count
+  end
+
+  def test_wait_non_retriable_code
+    perform_delay_count = 0
+    retry_policy = Gapic::Common::RetryPolicy.new retry_codes: [GRPC::Core::StatusCodes::RESOURCE_EXHAUSTED]
+    retry_policy.define_singleton_method :perform_delay! do
+      perform_delay_count += 1
+    end
+    polling_harness = Gapic::Common::PollingHarness.new retry_policy: retry_policy
+    assert_raises ::GRPC::Aborted do
+      polling_harness.wait do
+        raise ::GRPC::Aborted
+      end
+    end
+    assert_equal 0, perform_delay_count
+  end
+
+  def test_wait_argument_error
+    retry_policy = Gapic::Common::RetryPolicy.new
+    polling_harness = Gapic::Common::PollingHarness.new retry_policy: retry_policy
+    assert_raises ArgumentError do
+      polling_harness.wait
+    end
+  end
+end
---
+++ gapic-common/test/gapic/common/retry_policy_test.rb
@@ -0,0 +1,61 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "gapic/common/retry_policy"
+
+# Test class for Gapic::Common::RetryPolicy
+class RetryPolicyTest < Minitest::Test
+  def test_init_with_values
+    retry_policy = Gapic::Common::RetryPolicy.new(
+      initial_delay: 2, max_delay: 20, multiplier: 1.7,
+      retry_codes: [GRPC::Core::StatusCodes::UNAVAILABLE], timeout: 600
+    )
+    assert_equal 2, retry_policy.initial_delay
+    assert_equal 20, retry_policy.max_delay
+    assert_equal 1.7, retry_policy.multiplier
+    assert_equal [GRPC::Core::StatusCodes::UNAVAILABLE], retry_policy.retry_codes
+    assert_equal 600, retry_policy.timeout
+  end
+
+  def test_perform_delay_increment_delay
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 1, max_delay: 5, multiplier: 1.3
+    retry_policy.perform_delay!
+    refute_equal retry_policy.initial_delay, retry_policy.delay
+    assert_equal 1.3, retry_policy.delay
+  end
+
+  def test_perform_delay_retry_common_logic
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 3, max_delay: 10, multiplier: 2
+    retry_policy.define_singleton_method :retry? do
+      true
+    end
+    retry_policy.perform_delay
+    assert_equal 6, retry_policy.delay
+  end
+
+  def test_perform_delay_retry_error_logic
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 5, max_delay: 30, multiplier: 3
+    retry_policy.define_singleton_method :retry_error? do |_error|
+      true
+    end
+    retry_policy.perform_delay
+    assert_equal 15, retry_policy.delay
+  end
+
+  def test_max_delay_limit
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 10, max_delay: 12, multiplier: 1.5
+    retry_policy.perform_delay!
+    assert_equal 12, retry_policy.delay
+  end
+end
