a158fdecced072d0b43034d4196e2a0997d186e7
Daniel Azuma <dazuma@google.com>
2024-12-26 20:06:24 +0000

    chore: Kokoro job to release the generator gems on demand

---
+++ .kokoro/release-generators.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# Install gems in the user directory because the default install directory
+# is in a read-only location.
+export GEM_HOME=$HOME/.gem
+export PATH=$GEM_HOME/bin:$PATH
+
+gem install --no-document toys
+git fetch --tags
+git switch gapic-generator/v$GEM_VERSION
+toys release perform -v --all=gapic-generator < /dev/null
