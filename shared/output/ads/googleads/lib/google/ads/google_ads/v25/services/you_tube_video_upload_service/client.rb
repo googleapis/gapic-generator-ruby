a853d569a05129a4652652b1fffd7c92e48cb3f4
Viacheslav Rostovtsev <virost@google.com>
2026-09-25 20:16:47 +0000

    fix: send REST transport version for resumable uploads on gRPC clients
    Upload methods on a gRPC client travel over REST, so x-goog-api-client must report rest/<version> rather than the grpc/<version> default of Gapic::Headers.x_goog_api_client.

--- shared/output/ads/googleads/lib/google/ads/google_ads/v25/services/you_tube_video_upload_service/client.rb
+++ shared/output/ads/googleads/lib/google/ads/google_ads/v25/services/you_tube_video_upload_service/client.rb
@@ -261,7 +261,8 @@ module Google
                 # Set x-goog-api-client, x-goog-user-project and x-goog-api-version headers
                 metadata[:"x-goog-api-client"] ||= ::Gapic::Headers.x_goog_api_client \
                   lib_name: @config.lib_name, lib_version: @config.lib_version,
-                  gapic_version: ::Google::Ads::GoogleAds::VERSION
+                  gapic_version: ::Google::Ads::GoogleAds::VERSION,
+                  transports_version_send: [:rest]
                 metadata[:"x-goog-api-version"] = API_VERSION unless API_VERSION.empty?
                 metadata[:"x-goog-user-project"] = @quota_project_id if @quota_project_id
 
